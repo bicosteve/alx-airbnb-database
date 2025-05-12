ac2b739cab6c310bd7329132d830a295c0464d30
bsteve <bicosteve4@gmail.com>
2025-05-12 09:43:29 +0300

    corrected left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,7 +7,18 @@ FROM
 
 -- 2. Write query using LEFT JOIN to retrieve all properties and their reviews including properties with no Reviews
 SELECT
-    *
+    Property.property_id,
+    Property.host_id,
+    Property.name,
+    Property.description,
+    Property.location,
+    Property.pricepernight,
+    Property.created_at,
+    Property.updated_at,
+    Review.review_id,
+    Review.property_id,
+    Review.user_id,
+    Review.rating
 FROM
     Property
     LEFT JOIN Review ON Property.property_id = Review.property_id;
