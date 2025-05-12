088f6324beda535bf296ec7308700958bc298b5d
bsteve <bicosteve4@gmail.com>
2025-05-12 09:23:07 +0300

    added inner join

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,8 @@
+-- 1. Write a query usin INNER JOIN to retrieve all bookings and respective users who made the booking
+SELECT
+    *
+FROM
+    Booking
+    INNER JOIN User
+WHERE
+    User.user_id = Booking.user_id;
